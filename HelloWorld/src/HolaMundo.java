599d2338a5de7b62ea62f2ddb4bd6a7a19d826bb
Felipe-SG14 <felipe.solano.gos@gmail.com>
2022-06-17 14:02:42 -0500

    HelloWorld en Java

---
+++ HelloWorld/src/HolaMundo.java
@@ -0,0 +1,11 @@
+// La clase principal tiene el mismo nombre del archivo
+// Utilizar sintaxis Camel Case
+public class HolaMundo {
+    // Método main
+    // public -> Se instancia el cualquier lugar
+    // void -> Valor del retorno
+    // Shortcut -> main + tab
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es: Felipe");
+    }
+}

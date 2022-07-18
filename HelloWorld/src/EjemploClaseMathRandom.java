6365748d17d4f207e529f60fda4053625f999e4a
Felipe-SG14 <felipe.solano.gos@gmail.com>
2022-07-18 13:08:25 -0500

    Clase Random numeros aleatorios

--- HelloWorld/src/EjemploClaseMathRandom.java
+++ HelloWorld/src/EjemploClaseMathRandom.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class EjemploClaseMathRandom {
     public static void main(String[] args) {
         
@@ -11,6 +13,19 @@ public class EjemploClaseMathRandom {
         random = Math.floor(random);
         System.out.println("random = " + random);
         System.out.println("colores[(int) random] = " + colores[(int) random]);
-                
+
+        // Random con Java Util
+        Random random1 = new Random();
+        // Entero aleatorio
+        int randomInt = random1.nextInt();
+        System.out.println("randomInt = " + randomInt);
+
+        // Aleatorio entre 0 y 7, 7 no se incluye
+        // Valores entre 15 y 24
+        int randomIntP = 15 + random1.nextInt(25 - 15);
+        System.out.println("randomIntP = " + randomIntP);
+        
+        int randomArr = random1.nextInt(colores.length);
+        System.out.println("colores[randomArr] = " + colores[randomArr]);
     }
 }

02ec500d0e356ec38e878dd772944a9024de0198
Felipe-SG14 <felipe.solano.gos@gmail.com>
2022-07-17 21:53:35 -0500

    Date Parse y Comparacion

--- HelloWorld/src/EjemploJavaUtilDateParse.java
+++ HelloWorld/src/EjemploJavaUtilDateParse.java
@@ -14,6 +14,30 @@ public class EjemploJavaUtilDateParse {
             Date fecha = format.parse("2020-01-14");
             System.out.println("fecha = " + fecha);
             System.out.println("fecha con formato = " + format.format(fecha));
+
+
+            // Comparando fechas
+            Date fecha2 = new Date();
+
+            System.out.println("fecha = " + fecha);
+            System.out.println("fecha2 = " + fecha2);
+            if (fecha.after(fecha2)){
+                System.out.println("Fecha del usuario es despues que fecha2");
+            } else if (fecha.before(fecha2)){
+                System.out.println("Fecha es anterior a fecha dos");
+            } else if (fecha.equals(fecha2)) {
+                System.out.println("Las fechas son iguales");
+            }
+
+            if (fecha.compareTo(fecha2) > 0){
+                System.out.println("Fecha es despues que fecha2");
+            } else if (fecha.compareTo(fecha2) < 0 ){
+                System.out.println("Fecha es anterior a fecha2");
+            } else if (fecha.compareTo(fecha2) == 0) {
+                System.out.println("Las fechas son iguales");
+            }
+
+
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
